a96ff8809acf561b373afe022e86363f4f01c837
KarlaJulyana <karla@solarz.com.br>
2024-07-08 23:58:07 -0300

    chore: install dependencies

--- api_gateway/settings.py
+++ api_gateway/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'gateway'
+    'rest_framework',
+    'corsheaders',
+    'gateway',
 ]
 
 MIDDLEWARE = [
