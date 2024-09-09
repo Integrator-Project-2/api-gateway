0fead2be7788d32f908821a7915812043d785337
Valéria <vvqf2016@gmail.com>
2024-09-09 14:04:14 -0300

    chore: created services proxy to medication reminder creation

--- gateway/urls.py
+++ gateway/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path, re_path
 
 from gateway.views.create_user_view import CreateUserView
-from gateway.views.medication_service_views import AmountReminderServiceProxy, MedicationReminderDueReminderServiceProxy, MedicationSearchServiceProxy, MedicationServiceProxy
+
+from gateway.views.medication_service_views import AmountReminderServiceProxy, MedicationReminderRecordsServiceProxy, MedicationReminderServiceProxy, MedicationServiceProxy, TakeMedicationServiceProxy, UpcomingReminderRecordServiceProxy
+
 from gateway.views.patient_managment_views import AssociateDoctorToPatientView, CreateMedicalPrescriptionView, DoctorPatientsListView, PatientMedicationsAPIView, PatientPrescriptionsAPIView
+
 from gateway.views.user_service_views import DoctorsServiceProxy, PatientsServiceProxy, RegisterDoctorView, RegisterPatientView, SearchPatientByCPFView
 
 
@@ -13,12 +16,13 @@ urlpatterns = [
     path('associate-doctor-patient/', AssociateDoctorToPatientView.as_view(), name='associate-doctor-patient'),
     path('create-medical-prescription/', CreateMedicalPrescriptionView.as_view(), name='create-medical-prescription'),
     re_path(r'^medications/(?P<path>.*)$', MedicationServiceProxy.as_view(), name='medication-service'),
-    re_path(r'^medications/search$', MedicationSearchServiceProxy.as_view(), name='medication-search-service'),
-    re_path(r'^medications/due-reminders$', MedicationSearchServiceProxy.as_view(), name='medication-search-service'),
     re_path(r'^doctors-service/(?P<path>.*)$', DoctorsServiceProxy.as_view(), name='doctors-service'),
     re_path(r'^patients-service/(?P<path>.*)$', PatientsServiceProxy.as_view(), name='patients-service'),
-    re_path(r'^medication-reminder/(?P<path>.*)$', MedicationReminderDueReminderServiceProxy.as_view(), name='medication-reminder-service'),
+    re_path(r'^medication-reminder-records/(?P<path>.*)$', MedicationReminderRecordsServiceProxy.as_view(), name='medication-reminder-records-service'),
+    re_path(r'^upcoming-reminder-record/(?P<path>.*)$', UpcomingReminderRecordServiceProxy.as_view(), name='upcoming-reminder'),
+    re_path(r'^take-medication/(?P<reminder_record_id>\d+)/$', TakeMedicationServiceProxy.as_view(), name='take-medication'),
     re_path(r'^amount-reminder/(?P<path>.*)$', AmountReminderServiceProxy.as_view(), name='amount-reminder-service'),
+    re_path(r'^medication-reminder/(?P<path>.*)$', MedicationReminderServiceProxy.as_view(), name='medication-reminder-service'),
     re_path(r'^pacients-service/search-by-cpf/(?P<path>.*)?$', SearchPatientByCPFView.as_view(), name='search-patient-by-cpf'),
     path('doctors/<int:doctor_id>/patients/', DoctorPatientsListView.as_view(), name='doctor-patients-list'),
     re_path(r'^prescriptions/patient/(?P<patient_id>\d+)/$', PatientPrescriptionsAPIView.as_view(), name='doctor-patients-list'),
--- gateway/views/medication_service_views.py
+++ gateway/views/medication_service_views.py
@@ -24,18 +24,39 @@ class MedicationServiceProxy(BaseProxyView):
         url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medications/{path}'
         return self.proxy('DELETE', url, request, *args, **kwargs)
     
-class MedicationSearchServiceProxy(BaseProxyView):
-    def get(self, request, path, *args, **kwargs):
-        medication_name = request.GET.get('name', '')
-        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medications/{path}'
-        return self.proxy('GET', url, request, *args, **kwargs)
+# class MedicationSearchServiceProxy(BaseProxyView):
+#     def get(self, request, path, *args, **kwargs):
+#         medication_name = request.GET.get('name', '')
+#         url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medications/{path}'
+#         return self.proxy('GET', url, request, *args, **kwargs)
     
-class MedicationReminderDueReminderServiceProxy(BaseProxyView):
+class MedicationReminderRecordsServiceProxy(BaseProxyView):
     def get(self, request, path, *args, **kwargs):
         patient_id = request.GET.get('patient_id', '')
-        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder/due_reminders/?patient_id={patient_id}/{path}'
+        reminder_id = request.GET.get('reminder_id', '')
+        
+        if patient_id:
+            url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder-record/{path}?patient_id={patient_id}'
+
+        elif reminder_id:
+            url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder-record/{path}?reminder_id={reminder_id}'
+
+        else:
+            url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder-record/{path}'
+        
         return self.proxy('GET', url, request, *args, **kwargs)
     
+class TakeMedicationServiceProxy(BaseProxyView):
+    def post(self, request, reminder_record_id, *args, **kwargs):
+        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder-record/{reminder_record_id}/take-medication/'
+        return self.proxy('POST', url, request, *args, **kwargs)
+class UpcomingReminderRecordServiceProxy(BaseProxyView):
+    def get(self, request, *args, **kwargs):
+        patient_id = request.GET.get('patient_id', '')
+        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder-record/upcoming/?patient_id={patient_id}'
+
+        return self.proxy('GET', url, request, *args, **kwargs)
+
 class AmountReminderServiceProxy(BaseProxyView):
     def get(self, request, path, *args, **kwargs):
         url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/amount-reminder/{path}'
@@ -56,3 +77,22 @@ class AmountReminderServiceProxy(BaseProxyView):
     def delete(self, request, path, *args, **kwargs):
         url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/amount-reminder/{path}'
         return self.proxy('DELETE', url, request, *args, **kwargs)
+    
+class MedicationReminderServiceProxy(BaseProxyView):
+    def get(self, request, path, *args, **kwargs):
+        patient_id = request.GET.get('patient_id', '')
+        if patient_id:
+            url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder/{path}?patient_id={patient_id}'
+        else:
+            url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder/{path}'
+
+        return self.proxy('GET', url, request, *args, **kwargs)
+
+    def post(self, request, path, *args, **kwargs):
+        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder/{path}'
+        return self.proxy('POST', url, request, *args, **kwargs)
+
+    def delete(self, request, path, *args, **kwargs):
+        url = f'{settings.MEDICATION_SERVICE_BASE_API_URL}/medication-reminder/{path}'
+        return self.proxy('DELETE', url, request, *args, **kwargs)
+    
