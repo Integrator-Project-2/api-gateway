8e082c5f7f6c28e311bc4c20f3602d160f50daa6
Valéria <vvqf2016@gmail.com>
2024-09-10 02:49:57 -0300

    chore: ajusted BaseProxyView

--- gateway/views/base_proxy_view.py
+++ gateway/views/base_proxy_view.py
@@ -5,7 +5,8 @@ from rest_framework.permissions import IsAuthenticated
 
 class BaseProxyView(APIView):
     permission_classes = [IsAuthenticated]
-    
+
     def proxy(self, method, url, request, *args, **kwargs):
         response = requests.request(method, url, json=request.data, headers=request.headers)
-        return HttpResponse(response.content, status=response.status_code, content_type=response.headers['Content-Type'])
+        content_type = response.headers.get('Content-Type', 'application/json')
+        return HttpResponse(response.content, status=response.status_code, content_type=content_type)
